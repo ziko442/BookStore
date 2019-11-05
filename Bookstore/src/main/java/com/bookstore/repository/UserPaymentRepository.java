a88ed0950a60e8649990ddfebe19ead3381182a1
zaki442 <zakinassi7@gmail.com>
2019-11-05 22:43:36 +0100

    first commit

---
+++ Bookstore/src/main/java/com/bookstore/repository/UserPaymentRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.domain.UserPayment;
+
+public interface UserPaymentRepository extends CrudRepository<UserPayment, Long> {
+
+	
+}
